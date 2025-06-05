7504b1f3bf1df05d1c4ec3ea6498b4f273fb961e
somecat97 <kemeng.zhu@rakuten.com>
2025-06-06 08:04:53 +0900

    Update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bg-gradient-to-br from-sky-50 to-pink-50">
 		<div class="max-w-screen-lg mx-auto bg-amber-50 relative backdrop-blur-3xl">
-			<!-- <NuxtImg src="/girl.png" width="1024" /> -->
+			<NuxtImg src="/girl.png" width="1024" />
 			<h1
 				class="absolute right-4 bottom-8 p-4 rounded-lg text-white font-bold text-6xl text-center backdrop-blur-xs"
 			>
