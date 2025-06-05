f028d41a6ad1a0e61b975c84bb96705771ba2694
somecat97 <kemeng.zhu@rakuten.com>
2025-06-06 08:44:48 +0900

    Update nuxt icon config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,4 +41,18 @@ export default defineNuxtConfig({
 			},
 		],
 	},
+	icon: {
+		clientBundle: {
+			scan: {
+				// note that when you specify those values, the default behavior will be overridden
+				globInclude: [
+					'components/**/*.vue',
+					'layouts/**/*.vue',
+					'pages/**/*.vue',
+					'app.vue',
+				],
+				globExclude: ['node_modules', 'dist', '.nuxt', '.output', '.data'],
+			},
+		},
+	},
 })
