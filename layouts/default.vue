d98f3826a17fc90667a2af7720bf572d37e35e67
somecat97 <kemeng.zhu@rakuten.com>
2025-06-25 11:12:14 +0900

    Update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
 	<div>
 		<AppHeader />
-		<slot />
+		<div>
+			<slot />
+		</div>
 		<AppFooter />
 	</div>
 </template>
--- pages/library/[...slug].vue
+++ pages/library/[...slug].vue
@@ -1,107 +1,103 @@
 <template>
-	<div>
-		<h1>{{ $t('library') }}</h1>
-		<UModal v-model:open="isModalOpen" :title="t('filter')">
-			<UButton :label="t('filter')" color="neutral" variant="subtle" />
-			<template #body>
-				<UTabs
-					:default-value="getActiveTab()"
-					:items="tabs"
-					class="w-full"
-					:ui="{
-						list: 'overflow-x-auto gap-2',
-						trigger: 'whitespace-nowrap text-nowrap min-w-fit',
-					}"
-				>
-					<template #content="{ item }">
-						<UForm
-							:state="condition"
-							:schema="schema"
-							@submit="onSubmit($event, item.value)"
-						>
-							<UFormField :label="t('status')">
-								<USelect
-									v-model="condition.status"
-									:items="statusList"
-									class="w-full"
-								/>
-							</UFormField>
-							<UButton type="submit" :label="t('filter')" />
-						</UForm>
-					</template>
-				</UTabs>
-			</template>
-		</UModal>
-		<div
-			class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto"
-		>
-			<UCard
-				v-for="record in records"
-				:key="record.id"
-				class="relative overflow-hidden aspect-[3/4]"
+	<h1>{{ $t('library') }}</h1>
+	<UModal v-model:open="isModalOpen" :title="t('filter')">
+		<UButton :label="t('filter')" color="neutral" variant="subtle" />
+		<template #body>
+			<UTabs
+				:default-value="getActiveTab()"
+				:items="tabs"
+				class="w-full"
 				:ui="{
-					body: 'p-0 sm:p-0 h-full',
+					list: 'overflow-x-auto gap-2',
+					trigger: 'whitespace-nowrap text-nowrap min-w-fit',
 				}"
 			>
-				<UCollapsible
+				<template #content="{ item }">
+					<UForm
+						:state="condition"
+						:schema="schema"
+						@submit="onSubmit($event, item.value)"
+					>
+						<UFormField :label="t('status')">
+							<USelect
+								v-model="condition.status"
+								:items="statusList"
+								class="w-full"
+							/>
+						</UFormField>
+						<UButton type="submit" :label="t('filter')" />
+					</UForm>
+				</template>
+			</UTabs>
+		</template>
+	</UModal>
+	<div
+		class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto"
+	>
+		<UCard
+			v-for="record in records"
+			:key="record.id"
+			class="relative overflow-hidden aspect-[3/4]"
+			:ui="{
+				body: 'p-0 sm:p-0 h-full',
+			}"
+		>
+			<UCollapsible
+				:ui="{
+					content:
+						'origin-top-right data-[state=open]:animate-[collapsible-scale-in_200ms_ease-out] data-[state=closed]:animate-[collapsible-scale-out_200ms_ease-out] overflow-hidden absolute inset-0 bg-elevated/95 text-toned z-10',
+				}"
+			>
+				<UButton
+					class="group absolute top-0 right-0 rotate-45 translate-x-1/2 -translate-y-1/2 size-16 z-20"
+					trailing-icon="i-lucide-arrow-down"
+					size="xl"
+					:color="getButtonColor(record.status)"
 					:ui="{
-						content:
-							'origin-top-right data-[state=open]:animate-[collapsible-scale-in_200ms_ease-out] data-[state=closed]:animate-[collapsible-scale-out_200ms_ease-out] overflow-hidden absolute inset-0 bg-elevated/95 text-toned z-10',
+						base: 'rounded-none items-end justify-center p-1',
+						trailingIcon:
+							'group-data-[state=open]:rotate-180 transition-transform duration-200',
 					}"
-				>
-					<UButton
-						class="group absolute top-0 right-0 rotate-45 translate-x-1/2 -translate-y-1/2 size-16 z-20"
-						trailing-icon="i-lucide-arrow-down"
-						size="xl"
-						:color="getButtonColor(record.status)"
-						:ui="{
-							base: 'rounded-none items-end justify-center p-1',
-							trailingIcon:
-								'group-data-[state=open]:rotate-180 transition-transform duration-200',
-						}"
-					/>
-					<template #content>
-						<div class="pt-8 px-4 pb-4">
-							<div class="flex gap-2 mb-2">
-								<UBadge v-for="tag in record.tags" :key="tag">{{ tag }}</UBadge>
-							</div>
-							<h2 class="font-bold text-lg mb-2">{{ record.title }}</h2>
-							<div class="flex gap-1 items-center">
-								<UIcon name="i-lucide-calendar-1" />{{ record.releaseDate }}
-							</div>
-							<div class="flex gap-1 items-center">
-								<UIcon name="i-lucide-calendar-check" />{{
-									record.completeDate
-								}}
-							</div>
+				/>
+				<template #content>
+					<div class="pt-8 px-4 pb-4">
+						<div class="flex gap-2 mb-2">
+							<UBadge v-for="tag in record.tags" :key="tag">{{ tag }}</UBadge>
 						</div>
-					</template>
-				</UCollapsible>
-				<UCarousel
-					v-slot="{ item }"
-					:items="record.images"
-					:ui="{
-						item: 'basis-full h-full',
-						container: 'items-center h-full',
-						viewport: 'h-full',
-					}"
-					class="h-full"
-				>
-					<div class="w-full h-full flex justify-center items-center">
-						<div
-							class="absolute inset-0 blur"
-							:style="{
-								backgroundImage: `url(${item})`,
-								backgroundSize: 'cover',
-								backgroundPosition: 'center',
-							}"
-						></div>
-						<NuxtImg :src="item" class="object-contain h-full w-full z-10" />
+						<h2 class="font-bold text-lg mb-2">{{ record.title }}</h2>
+						<div class="flex gap-1 items-center">
+							<UIcon name="i-lucide-calendar-1" />{{ record.releaseDate }}
+						</div>
+						<div class="flex gap-1 items-center">
+							<UIcon name="i-lucide-calendar-check" />{{ record.completeDate }}
+						</div>
 					</div>
-				</UCarousel>
-				<h2 class="font-bold text-center hidden">{{ record.title }}</h2>
-			</UCard>
-		</div>
+				</template>
+			</UCollapsible>
+			<UCarousel
+				v-slot="{ item }"
+				:items="record.images"
+				:ui="{
+					item: 'basis-full h-full',
+					container: 'items-center h-full',
+					viewport: 'h-full',
+				}"
+				class="h-full"
+			>
+				<div class="w-full h-full flex justify-center items-center">
+					<div
+						class="absolute inset-0 blur"
+						:style="{
+							backgroundImage: `url(${item})`,
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}"
+					></div>
+					<NuxtImg :src="item" class="object-contain h-full w-full z-10" />
+				</div>
+			</UCarousel>
+			<h2 class="font-bold text-center hidden">{{ record.title }}</h2>
+		</UCard>
 	</div>
 </template>
 <script setup lang="ts">
